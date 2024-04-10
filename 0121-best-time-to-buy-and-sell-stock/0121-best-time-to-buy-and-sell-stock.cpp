ed1f0433c538bdca0eda39f675ecab7007892343
manojsmkg <53961008+manojsmkg@users.noreply.github.com>
2024-04-10 08:42:05 +0530

    Time: 85 ms (63.61%), Space: 95.7 MB (94.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,19 +4,15 @@ public:
         int n=prices.size();
         vector<int> greatest;
         int greatestSoFar=prices[n-1];
-        greatest.push_back(greatestSoFar);
+        int maxProfit = 0;
         for(int i=n-2;i>=0;i--)
         {
             if(prices[i]>greatestSoFar)
                 greatestSoFar = prices[i];
-            greatest.push_back(greatestSoFar);
+            
+            maxProfit = max(maxProfit,greatestSoFar-prices[i]);
         }
-        reverse(greatest.begin(),greatest.end());
-        int maxProfit = 0;
-        for(int i=0;i<n;i++)
-        {
-            maxProfit = max(maxProfit,greatest[i]-prices[i]);
-        }
+
         return maxProfit;
   
     }
