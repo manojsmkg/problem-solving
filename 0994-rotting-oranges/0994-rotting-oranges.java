221d3e1865c5860bc83be660acca32bfae820902
manojsmkg <53961008+manojsmkg@users.noreply.github.com>
2024-07-08 15:07:27 +0530

    Time: 7 ms (7.04%), Space: 43.2 MB (7.18%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int orangesRotting(int[][] a) {
+        int count=0;
+        
+        Queue<ArrayList<Integer>> q = new LinkedList();
+        
+        int rows=a.length;
+        int columns=a[0].length;
+        int freshOranges=0;
+        
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<columns;j++)
+            {
+                if(a[i][j] == 2)
+                {
+                    q.add(new ArrayList<>(Arrays.asList(i,j)));
+                }
+                if(a[i][j]==1) freshOranges++;
+            }
+        }
+        System.out.println(freshOranges);
+        if(q.isEmpty() && freshOranges>0) return -1;
+        freshOranges=freshOranges+q.size();
+        while(!q.isEmpty())
+        {
+            int k=q.size();
+            while(k>0)
+            {
+                ArrayList<Integer> currIndices = q.poll();
+                k--;
+                freshOranges--;
+                int i=currIndices.get(0),j=currIndices.get(1);
+                if(i-1>=0 && a[i-1][j]==1) {a[i-1][j]=2;q.add(new ArrayList<>(Arrays.asList(i-1,j)));}
+                if(i+1<rows && a[i+1][j]==1) {a[i+1][j]=2;q.add(new ArrayList<>(Arrays.asList(i+1,j)));}
+                if(j-1>=0 && a[i][j-1]==1) {a[i][j-1]=2;q.add(new ArrayList<>(Arrays.asList(i,j-1)));}
+                if(j+1<columns && a[i][j+1]==1) {a[i][j+1]=2;q.add(new ArrayList<>(Arrays.asList(i,j+1)));}
+            }
+            count++;
+        }
+        System.out.println(freshOranges);
+        if(freshOranges==0) return count>0?count-1:count;
+        return -1;
+        
+        
+        
+    }
+}
