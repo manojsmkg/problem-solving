7f44fa160a9c67b7c59f4971b289957846994f44
manojsmkg <53961008+manojsmkg@users.noreply.github.com>
2024-11-14 07:35:12 +0530

    Time: 7 ms (7.36%), Space: 47.6 MB (33.83%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,24 +1,28 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
+    public int minPathSum(int[][] grid) 
+    {
+        int m = grid.length;
+        int n = grid[0].length;
         
-        int minScore[][] = new int[m][n];
-        for(int i=0;i<m;i++)
+         int dp[][] = new int[m][n];
+         dp[0][0] = grid[0][0];
+        
+        for(int i=1;i<m;i++) dp[i][0] = dp[i-1][0] + grid[i][0];
+        for(int j=1;j<n;j++) dp[0][j] = dp[0][j-1] + grid[0][j];
+        int[][] allDirs = {{-1,0},{0,-1}};
+        
+        for(int i=1;i<m;i++)
         {
-            for(int j=0;j<n;j++) {
-                
-            if(i==0&&j==0) minScore[i][j]=grid[i][j];
-            else if(i==0)  minScore[i][j]=minScore[i][j-1]+grid[i][j];
-            else if(j==0) minScore[i][j]=minScore[i-1][j]+grid[i][j];
-                else{
-            int up=0,left=0;
-            if(i-1>=0) up=minScore[i-1][j];
-            if(j-1>=0) left=minScore[i][j-1];
-            minScore[i][j]=Math.min(up,left)+grid[i][j];
-                }
-        }}
-
-     return minScore[m-1][n-1];   
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int ind=0;ind<allDirs.length;ind++)
+                if(i+allDirs[ind][0]>=0 && i+allDirs[ind][0] < m && j+allDirs[ind][1] >=0 && j+allDirs[ind][1] < n)
+                   dp[i][j] = Math.min(dp[i][j] , dp[i+allDirs[ind][0]][j+allDirs[ind][1]] + grid[i][j]);
+            }
+        }
+       
+        return dp[m-1][n-1];
+        
     }
 }
