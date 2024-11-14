906db15ab5cf703a2e489a22425399d2683645aa
manojsmkg <53961008+manojsmkg@users.noreply.github.com>
2024-11-14 07:25:24 +0530

    Time: 1 ms (9.23%), Space: 40.5 MB (38.70%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,19 +1,22 @@
-class Solution {
-    public int uniquePaths(int m, int n) {
+class Solution 
+{
+    public int uniquePaths(int m, int n) 
+    {
         int dp[][] = new int[m][n];
-        dp[0][0]=1;
-
-        for(int i=0;i<m;i++)
+        
+        for(int i=0;i<m;i++) dp[i][0] = 1;
+        for(int j=0;j<n;j++) dp[0][j] = 1;
+        int[][] allDirs = {{-1,0},{0,-1}};
+        
+        for(int i=1;i<m;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=1;j<n;j++)
             {
-                if(i==0&&j==0) {dp[i][j]=1;continue;}
-                int up=0,left=0;
-                if(i>0) up=dp[i-1][j];
-                if(j>0) left = dp[i][j-1];
-                dp[i][j]=up+left;
+                for(int ind=0;ind<allDirs.length;ind++)
+                if(i+allDirs[ind][0]>=0 && i+allDirs[ind][0] < m && j+allDirs[ind][1] >=0 && j+allDirs[ind][1] < n)
+                   dp[i][j] = dp[i][j] + dp[i+allDirs[ind][0]][j+allDirs[ind][1]];
             }
-        }  
+        }
         return dp[m-1][n-1];
     }
 }
