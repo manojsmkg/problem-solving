d3afdc147b91a5705b9f33ce088d95f80998c1df
manojsmkg <53961008+manojsmkg@users.noreply.github.com>
2024-11-15 07:26:46 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (69.57%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -7,9 +7,11 @@
  * }
  */
 class Solution {
-    public void deleteNode(ListNode node) {
-        if(node==null || node.next == null) return;
-        node.val = node.next.val;
-        node.next = node.next.next;
+    public void deleteNode(ListNode node) 
+    {
+      
+       node.val = node.next.val;
+       node.next = node.next.next;
+        
     }
 }
