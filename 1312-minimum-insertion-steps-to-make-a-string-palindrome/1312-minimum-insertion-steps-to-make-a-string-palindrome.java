fa09f05f82d7ca32068e899a617cbda96e4eee64
manojsmkg <53961008+manojsmkg@users.noreply.github.com>
2024-11-18 22:42:23 +0530

    Time: 22 ms (71.96%), Space: 44.9 MB (89.58%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -4,6 +4,8 @@ class Solution {
         int n = s.length();
         
         int[][] dp = new int[n+1][n+1];
+        int[] curr = new int[n+1];
+        int[] prev = new int[n+1];
         
         for(int i=1;i<=n;i++)
         {
@@ -11,12 +13,13 @@ class Solution {
             {
                 if(s.charAt(i-1) == s.charAt(n-j))
                 {
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                    curr[j] = prev[j-1] + 1;
                 }
                 else
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                    curr[j] = Math.max(prev[j],curr[j-1]);
             }
+            prev = curr.clone();
         }
-        return n - dp[n][n];
+        return n - curr[n];
     }
 }
