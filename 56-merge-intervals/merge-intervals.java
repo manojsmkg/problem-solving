3e20748af7393bc781aab9dfa59d4b8b5ab98450
manojsmkg <53961008+manojsmkg@users.noreply.github.com>
2024-12-30 11:30:56 +0530

    Time: 6 ms (98.16%) | Memory: 46.6 MB (57.20%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[][] merge(int[][] intervals) 
+    {
+        int n = intervals.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b)->a[0]-b[0]);
+        for(int i=0;i<n;i++)
+        {
+            pq.add(intervals[i]);
+        }
+        int start = pq.peek()[0]; 
+        int end = pq.peek()[1]; 
+        List<int[]> result = new ArrayList<>();
+        while(pq.isEmpty()==false)
+        {
+            int[] currInterval = pq.poll();
+            if(currInterval[0]<=end) 
+            {
+                end = Math.max(end,currInterval[1]);
+            }
+            else 
+            {
+                int[] mergedInterval = {start,end};
+                result.add(mergedInterval);
+                start = currInterval[0];
+                end = currInterval[1];
+            }
+        }
+        
+        int[] mergedInterval = {start,end};
+        result.add(mergedInterval);
+        int[][] resultArr = new int[result.size()][2];
+        for(int i=0;i<result.size();i++)
+        {
+
+            resultArr[i][0] = result.get(i)[0];
+            resultArr[i][1] = result.get(i)[1];
+        }
+        return resultArr;
+    }
+
+        
+    }
