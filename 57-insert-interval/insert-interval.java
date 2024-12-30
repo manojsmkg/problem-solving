3334664a7eee637eb0732f4f04dbaec46ade1f61
manojsmkg <53961008+manojsmkg@users.noreply.github.com>
2024-12-30 11:26:45 +0530

    Time: 4 ms (12.23%) | Memory: 45.1 MB (30.77%) - LeetSync

---
+++ 57-insert-interval/insert-interval.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) 
+    {
+        int n = intervals.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b)->a[0]-b[0]);
+        for(int i=0;i<n;i++)
+        {
+            pq.add(intervals[i]);
+        }
+        pq.add(newInterval);
+        int start = pq.peek()[0]; 
+        int end = pq.peek()[1]; 
+     //   pq.poll();
+        List<int[]> result = new ArrayList<>();
+        int lastAdded = 0;
+        int processInterval = 0;
+        while(pq.isEmpty()==false)
+        {
+            int[] currInterval = pq.poll();
+            if(currInterval[0]<=end) 
+            {
+                end = Math.max(end,currInterval[1]);
+                lastAdded = 1;
+            }
+            else 
+            {
+                int[] mergedInterval = {start,end};
+                result.add(mergedInterval);
+                start = currInterval[0];
+                end = currInterval[1];
+                processInterval = 1;
+            }
+        }
+        
+        if(lastAdded == 1 || processInterval == 1) {
+            int[] mergedInterval = {start,end};
+            result.add(mergedInterval);
+            }
+        int[][] resultArr = new int[result.size()][2];
+        for(int i=0;i<result.size();i++)
+        {
+
+            resultArr[i][0] = result.get(i)[0];
+            resultArr[i][1] = result.get(i)[1];
+        }
+        return resultArr;
+    }
+}
