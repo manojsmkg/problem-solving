8a80bd6bf123c4ff878e85431a9a60f86f812748
manojsmkg <53961008+manojsmkg@users.noreply.github.com>
2025-01-26 15:30:29 +0530

    Time: 485 ms (5.27%) | Memory: 62.4 MB (5.99%) - LeetSync

---
+++ 134-gas-station/gas-station.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) 
+    {
+        int n=gas.length;
+        int totalEffectiveGas = 0;
+        int effectiveGas[] = new int[n];
+        int s = 0;
+        for(int i=0;i<n;i++)
+        {
+            totalEffectiveGas += gas[i]-cost[i];
+            effectiveGas[i] = (gas[i]-cost[i]);
+        }
+        if(totalEffectiveGas<0) return -1;
+
+        int prefixSum = 0;
+        int suffixSum[] = new int[n];
+        for(int i=n-1;i>=0;i--)
+        {
+            suffixSum[i] = s;
+            s+=effectiveGas[i];
+        }
+        
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            prefixSum+=effectiveGas[i];
+            System.out.print(prefixSum+ " ");
+            if(prefixSum<0 ) {prefixSum=0;ans = i+1;}
+        }
+        return ans;
+    }
+}
