15d0396b0e6b793cb51772469e212473bb765728
manojsmkg <53961008+manojsmkg@users.noreply.github.com>
2025-01-28 10:33:37 +0530

    Time: 5 ms (65.83%) | Memory: 45 MB (57.42%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -1,17 +1,17 @@
 class Solution {
 
-    int fishCollected(int[][] grid,int x,int y,int m,int n,int[][] visited)
+    int fishCollected(int[][] grid,int x,int y,int m,int n)
     {
-        visited[x][y] = 1;
         int xDir[] = {0,0,1,-1};
         int yDir[] = {1,-1,0,0};
         int fishCount = grid[x][y];
+        grid[x][y] = 0;
         for(int i=0;i<4;i++)
         {
             int newX = x+xDir[i];
             int newY = y+yDir[i];
-            if(newX>=0 && newX<m && newY>=0 && newY<n && grid[newX][newY]!=0 && visited[newX][newY]==0)
-                fishCount+=fishCollected(grid,newX,newY,m,n,visited);
+            if(newX>=0 && newX<m && newY>=0 && newY<n && grid[newX][newY]!=0)
+                fishCount+=fishCollected(grid,newX,newY,m,n);
         }
         return fishCount;
 
@@ -19,14 +19,13 @@ class Solution {
     public int findMaxFish(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] visited = new int[m][n];
         int maxFish = 0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j]!=0 && visited[i][j]==0)
-                    maxFish = Math.max(maxFish,fishCollected(grid,i,j,m,n,visited));
+                if(grid[i][j]!=0)
+                    maxFish = Math.max(maxFish,fishCollected(grid,i,j,m,n));
             }
         }
         return maxFish;
